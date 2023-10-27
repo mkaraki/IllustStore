ed019011d1bf71563c6e90ca06650bc756fa19e5
mkaraki <26180919+mkaraki@users.noreply.github.com>
2023-10-27 12:33:28 +0900

    Add word cloud style tag indicate

--- imageViewer/views/tags.php
+++ imageViewer/views/tags.php
@@ -1,3 +1,29 @@
+<?php
+$res = DB::query(
+    'SELECT
+        tA.tagId AS id,
+        t.tagName,
+        COUNT(tA.tagId) AS count
+    FROM
+        tags t,
+        tagAssign tA
+    WHERE
+        tA.tagId = t.id
+    GROUP BY tA.tagId
+    ORDER BY tagName'
+);
+
+$tags = DB::queryFirstField(
+    'SELECT COUNT(id) FROM tags'
+);
+
+$maxCount = DB::queryFirstField(
+    'SELECT COUNT(tagId) FROM tagAssign GROUP BY tagId'
+);
+
+$sizeDiff = 20.0 / $maxCount;
+$initSize = 12.0;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,11 +35,15 @@
 </head>
 
 <body>
-    <ul class="forever-ul">
-        <?php foreach (DB::query('SELECT id, tagName FROM tags ORDER BY tagName') as $v) : ?>
-            <li><a href="/tag/<?= $v['id'] ?>"><?= htmlentities($v['tagName']) ?></a></li>
+    <ul class="forever-ul tag-cloud">
+        <?php foreach ($res as $v) : ?>
+            <li><a href="/tag/<?= $v['id'] ?>" style="font-size: <?= $initSize + ($sizeDiff * $v['count']) ?>pt"><?= htmlentities($v['tagName']) ?></a> (<?= $v['count'] ?>)</li>
         <?php endforeach; ?>
     </ul>
+    <footer>
+        <?= $tags ?> tags found.<br />
+        MAX <?= $maxCount ?> times used.
+    </footer>
 </body>
 
 </html>
