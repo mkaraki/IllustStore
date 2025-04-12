a8ea088170fda3a2be5fdddecd1201f48e2bf45a
mkaraki <26180919+mkaraki@users.noreply.github.com>
2025-04-12 23:56:37 +0900

    Fix SQL

--- imageViewer/routes/image.php
+++ imageViewer/routes/image.php
@@ -53,25 +53,27 @@ $klein->respond('GET', '/image/[i:imageId]', function ($request, $response, $ser
 
     $neighbor_image = DB::query('SELECT 
             tA2.illustId AS id,
-            i.width AS width,
-            i.height AS height,
+            i.width,
+            i.height,
             COUNT(*) AS tag_match_count
         FROM 
             tagAssign tA1
-            illusts i
         JOIN 
             tagAssign tA2
         ON
             tA1.tagId = tA2.tagId
+        JOIN
+            illusts i
+        ON
+            tA2.illustId = i.id
         WHERE 
             tA1.illustId = %i
             AND tA2.illustId != %i
-            AND tA2.illustId = i.id
         GROUP BY 
             tA2.illustId
         ORDER BY 
             tag_match_count DESC
-        LIMIT 10;',
+        LIMIT 10',
         $request->imageId,
         $request->imageId
     );
