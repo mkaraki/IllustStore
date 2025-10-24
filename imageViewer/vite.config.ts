916b390d8f0a4552f4cf212e8abe1463f0717a05
mkaraki <git@su.mkarakiapps.com>
2025-10-24 14:07:12 +0900

    Add sentry release

--- imageViewer/vite.config.ts
+++ imageViewer/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import {sentryVitePlugin} from "@sentry/vite-plugin";
 
 export default defineConfig({
     build: {
@@ -25,6 +26,12 @@ export default defineConfig({
         }),
         wayfinder({
             formVariants: true,
+        }),
+        sentryVitePlugin({
+            org: process.env.SENTRY_ORG,
+            project: process.env.SENTRY_PROJECT,
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            url: process.env.SENTRY_URL ?? 'https://sentry.io/',
         }),
     ],
 });
